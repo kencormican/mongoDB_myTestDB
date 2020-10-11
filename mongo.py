a6df544819bd50040ecaf60da042ec7e28361d32
kencormican <ken.cormican@bt.com>
2020-10-11 10:51:57 +0000

    Run Mongo Commands From a Python lesson code

---
+++ mongo.py
@@ -0,0 +1,66 @@
+import os
+import pymongo
+if os.path.exists("env.py"):
+    import env
+
+
+MONGO_URI = os.environ.get("MONGO_URI")
+DATABASE = "myTestDB"
+COLLECTION = "celebrities"
+
+
+def mongo_connect(url):
+    try:
+        conn = pymongo.MongoClient(url)
+        print("Mongo is connected")
+        return conn
+    except pymongo.errors.ConnectionFailure as e:
+        print("Could not connect to MongoDB: %s") % e
+
+
+conn = mongo_connect(MONGO_URI)
+
+coll = conn[DATABASE][COLLECTION]
+
+
+# """ Insert multipe documents """
+# new_docs = [{
+#     "first": "terry",
+#     "last": "pratchett",
+#     "dob": "28/04/1948",
+#     "gender": "m",
+#     "hair_color": "not much",
+#     "occupation": "writer",
+#     "nationality": "british"
+# }, {
+#     "first": "george",
+#     "last": "rr martin",
+#    "dob": "20/09/1948",
+#     "gender": "m",
+#     "hair_color": "white",
+#     "occupation": "writer",
+#     "nationality": "american"
+# }]
+
+# coll.insert_many(new_docs)
+
+# """ Find documents with 'first' name set to 'douglas' """
+# documents = coll.find({"first": "douglas"})
+
+# """ Delete documents with 'first' name set to 'douglas' """
+# coll.remove({"first": "douglas"})
+
+# documents = coll.find()
+
+""" Update all documents """
+coll.update_many(
+    {"nationality": "american"},
+    {"$set": {"hair_colour": "maroon"}}
+)
+
+
+documents = coll.find({"nationality": "american"})
+
+
+for doc in documents:
+    print(doc)
